5ccc49a915bdff2d67807223cf4ac74b2c49bda7
Paulo Estrela <dt.paulohenrique@gmail.com>
2025-05-25 18:04:49 -0300

    first commit

---
+++ src/main/java/com/github/ConsultaFIPE/ConsultaFipeApplication.java
@@ -0,0 +1,13 @@
+package com.github.ConsultaFIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeApplication.class, args);
+	}
+
+}
